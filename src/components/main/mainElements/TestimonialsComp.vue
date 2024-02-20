f6c7aeb68aaa0763be167955c0fed9d5bc487c9d
marcovittoriozacca <zaccamarco01@gmail.com>
2024-02-20 19:09:34 +0100

    fix: max width on all the elements

--- src/components/main/mainElements/TestimonialsComp.vue
+++ src/components/main/mainElements/TestimonialsComp.vue
@@ -30,9 +30,17 @@ export default{
 
 <template>
 
-    <div class="testimonials">
+    <div class="testimonials position-relative">
+        <div class="my-pagination d-flex align-items-center column-gap-2 text-secondary">
+            <span>0{{ currentSlide + 1}}</span>
+            <div class="progress-bar rounded-pill">
+                <div class="inner-progress-bar rounded-pill" :style="{ 'width': ((currentSlide + 1) / testimonials.length) * 100 + '%'  }" ></div>
+            </div>
+            <span v-if="testimonials.length < 10">0{{ testimonials.length }}</span>
+            <span v-else>{{ testimonials.length }}</span>
+        </div>
 
-        <Carousel ref="carousel" v-model="currentSlide" snapAlign="start" :wrapAround="true" class="position-relative z-2">
+        <Carousel ref="carousel" v-model="currentSlide" snapAlign="start" :wrapAround="true" :autoplay="5000" :transition="500" :pauseAutoplayOnHover="true" class="position-relative z-2">
             <Slide v-for="testimonial in testimonials" :key="testimonial" class="text-white">
                 <div class="d-flex flex-column testimonial-info row-gap-4">
                     <figure class="m-0">
@@ -42,13 +50,7 @@ export default{
                     <q class="opacity-75">
                         {{ testimonial.description }}
                     </q>
-                    <div class="my-pagination d-flex align-items-center column-gap-2">
-                        <span>{{ currentSlide + 1}}</span>
-                        <div class="progress-bar rounded-pill">
-                            <div class="inner-progress-bar rounded-pill" :style="{ 'width': ((currentSlide + 1) / testimonials.length) * 100 + '%'  }" ></div>
-                        </div>
-                        <span>{{ testimonials.length }}</span>
-                    </div>
+
                 </div>   
             </Slide>
   
@@ -56,11 +58,13 @@ export default{
                 
                 <Navigation>
 
+                    <!-- right arrow -->
                     <template #next>
                             <span>
                                 <i class="fas fa-arrow-right-long"></i>
                             </span>
                     </template>
+
                     <!-- left arrows -->
                     <template #prev>
                         <span>
@@ -107,8 +111,15 @@ export default{
             h4{
                 font-size: 1.1rem;
             }
-            .my-pagination{
+            
+
+        }
+        .my-pagination{
                 margin: 0 auto;
+                position: absolute;
+                bottom: 50px;
+                left: 50%;
+                transform: translateX(-50%);
                 .progress-bar{
                     width: 120px;
                     height: 1.5px;
@@ -119,8 +130,12 @@ export default{
                     }
                 }
             }
-
-        }
     }
+
+    .carousel__prev,
+.carousel__next {
+  box-sizing: content-box;
+  border: 5px solid white;
+}
 </style>
 
